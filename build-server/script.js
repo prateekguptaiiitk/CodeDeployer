2b2d4d26915db862318d00e3db7dfa6eface9a1f
prtk_gupta <prateekjpg@gmail.com>
2025-06-29 14:37:44 +0530

    Add code to build server

---
+++ build-server/script.js
@@ -0,0 +1,78 @@
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const mime = require("mime-types");
+const Redis = require("ioredis");
+
+const publisher = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD
+});
+
+const s3Client = new S3Client({
+    region: process.env.S3_REGION,
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+
+const PROJECT_ID = process.env.PROJECT_ID;
+
+function publishLog(log) {
+  publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }));
+}
+
+async function init() {
+  console.log("Executing script.js");
+  publishLog("Build Started...");
+  const outDirPath = path.join(__dirname, "output");
+
+  const p = exec(`cd ${outDirPath} && npm install && npm run build`);
+
+  p.stdout.on("data", function (data) {
+    console.log(data.toString());
+    publishLog(data.toString());
+  });
+
+  p.stdout.on("error", function (data) {
+    console.log("Error", data.toString());
+    publishLog(`error: ${data.toString()}`);
+  });
+
+  p.on("close", async function () {
+    console.log("Build Complete");
+    publishLog(`Build Complete`);
+    const distFolderPath = path.join(__dirname, "output", "dist");
+    const distFolderContents = fs.readdirSync(distFolderPath, {
+      recursive: true,
+    });
+
+    publishLog(`Starting to upload`);
+    for (const file of distFolderContents) {
+      const filePath = path.join(distFolderPath, file);
+      if (fs.lstatSync(filePath).isDirectory()) continue;
+
+      console.log("uploading", filePath);
+      publishLog(`uploading ${file}`);
+
+      const command = new PutObjectCommand({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `__outputs/${PROJECT_ID}/${file}`,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath),
+      });
+
+      await s3Client.send(command);
+      publishLog(`uploaded ${file}`);
+      console.log("uploaded", filePath);
+    }
+    publishLog(`Done`);
+    console.log("Done...");
+  });
+}
+
+init();
